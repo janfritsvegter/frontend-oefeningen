c292dd449c541acf62064ba5b7468f959c188f59
janfritsvegter <jan.frits.vegter@gmail.com>
2021-10-16 10:03:40 +0200

    methods

--- frontend-javascript-methods/date-methods.js
+++ frontend-javascript-methods/date-methods.js
@@ -13,8 +13,36 @@ console.log(currentDay);
 // Uren: 16 (als er een vier in de klok zit op het moment van aanroepen)
 // Minuten: 36 (als het 16:36 is op het moment van aanroepen)
 
-
+const hours = currentDay.getHours();
+const minutes = currentDay.getMinutes();
+console.log(hours);
+console.log(minutes);
 
 /* Opdracht 2 */
 // Format het date-object dat je bij de vorige vraag hebt gemaakt naar een leesbare datum
 // ---- Verwachte uitkomst: Tue Apr 06 2021 (afhankelijk van het moment)
+
+let readableDate = currentDay.toDateString();
+console.log(readableDate);
+
+// Nederlandse versie voluit
+const longOptions = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+};
+
+readableDate = currentDay.toLocaleDateString('nl-NL', longOptions); // geeft zaterdag 26 december 1992
+console.log(readableDate);
+
+// Nederlandse versie kort
+const shortOptions = {
+    weekday: 'short',
+    month: 'short',
+    day: 'numeric',
+
+};
+
+readableDate = currentDay.toLocaleDateString('nl-NL', shortOptions); // geeft zaterdag 26 december 1992
+console.log(readableDate);
--- frontend-javascript-methods/math-methods.js
+++ frontend-javascript-methods/math-methods.js
@@ -9,18 +9,18 @@ console.log(Math.PI);
 // Tip: de formule is omtrek = diameter * pi
 // ---- Verwachte uitkomst: 12.566370614359172
 
-
+console.log(Math.PI*4);
 /* Opdracht 2: */
 // Bereken de omtrek van een cirkel met een diameter van 12 en rond dit af naar een heel getal. Log dit in de terminal.
 // ---- Verwachte uitkomst: 38
 
-
+console.log(Math.round(Math.PI*12));
 
 /* Opdracht 3: */
 // Bereken de wortel van 9 en log dit in de terminal
 // ---- Verwachte uitkomst: 3
 
-
+console.log(Math.sqrt(9));
 
 /* Opdracht 4: */
 // Genereer een willekeurig getal tussen de 0 en 1 en doe dan een check:
@@ -28,3 +28,31 @@ console.log(Math.PI);
 // * is het willekeurige getal kleiner dan 0.5 ? Log dan het getal met "Het glas is half leeg" in de terminal
 // Tip: iedere keer als je het script opnieuw draait (wel even een extra spatie toevoegen o.i.d.) is dit getal anders ;)
 // ---- Verwachte uitkomst: Bijbehorende zin afhankelijk van de uitkomst
+
+for (let i = 0; i < 100; i++) {
+    const rand = Math.random();
+    if (rand>0.5){
+        console.log(rand +" Het glas is half vol");
+    }else{
+        console.log(rand +" Het glas is half leeg");
+    }
+}
+const arr = [0,0,0,0,0,0,0,0,0,0,0,0];
+for (let i = 0; i < 10000; i++) {
+    const rand = Math.round(Math.random()*10);
+    arr[rand]++;
+}
+console.log(arr);
+for (let i = 0; i < arr.length; i++) {
+    console.log(i+" "+arr[i]);
+}
+
+const arr2 = [0,0,0,0,0,0,0,0,0,0,0,0];
+for (let i = 0; i < 1000000; i++) {
+    const rand = Math.round((Math.random()*10)+0.5);
+    arr2[rand]++;
+}
+console.log(arr2);
+for (let i = 0; i < arr2.length; i++) {
+    console.log(i+" "+arr2[i]);
+}
